bf1326f41b1818ea0332a44fd60b4408a7b6dd9c
reXt88 <seva88best.com@yandex.ru>
2022-02-07 13:16:57 +0000

    settings_neplan

--- settings_netplan.py
+++ settings_netplan.py
@@ -2,12 +2,39 @@
 import os
 import os.path
 
-with open("/etc/netplan/00-installer-config.yaml", "w") as f:
-	f.write('''network:
+print("static or dynamic?")
+print("   1         2")
+a = input()
+if a == 2:
+	with open("/etc/netplan/00-installer-config.yaml", "w") as f:
+		f.write('''network:
   ethernets:
     enp0s3:
       dhcp4: yes
     enp0s8:
       dhcp4: yes
   version: 2''')
+elif a == 1:
+	devices = input("How many devices do you have ")
+	if devices == 1:
+		addr1 = input("Enter address for enp0s3 ")
+		with open ("/etc/netplan/00-insaller-config.yaml", "w") as f:
+			f.write(f'''network:
+  ethernets:
+    enp0s3:
+      dhcp4: no
+      addresses: [{addr1}]
+''')
+	if devices == 2:
+		addr1 = input("Enter address for enp0s3 ")
+		addr2 = input("Enter address for enp0s8 ")
+	with open("/etc/netplan/00-installer-config.yaml", "w") as f:
+		f.write(f'''network:
+  ethernets:
+    enp0s3:
+      dhcp4: no
+      addresses: [{aadr1}]
+    enp0s9:
+      dhcp4: no
+      addresses: [{addr2}]''')
 os.system("netplan apply")

c54078df7528d5a367e25fe6b80f95fe948e8676
reXt88 <seva88best.com@yandex.ru>
2022-02-22 12:27:48 +0000

    Run this if you have ifupdown

--- settings_ifupdown.py
+++ settings_ifupdown.py
@@ -5,9 +5,9 @@ import os
 print("static or dynamic")
 print("   1         2")
 a = input()
-if a == "2"
-with open("/etc/network/interfaces", "w") as f:
-	f.write('''auto enp0s3
+if a == "2":
+	with open("/etc/network/interfaces", "w") as f:
+		f.write('''auto enp0s3
 iface enp0s3 inet dhcp
 
 auto enp0s8
@@ -24,9 +24,9 @@ iface eth0 inet static
 	netmask {netmask1}''')
 	if devices == "2":
 		addr1 = input("Enter address for enp0s3 ")
-                netmask1 = input("Enter netmask for enp0s3 ")
+		netmask1 = input("Enter netmask for enp0s3 ")
 		addr2 = input("Enter address for enp0s8 ")
-                netmask2 = input("Enter netmask for enp0s8 ")
+		netmask2 = input("Enter netmask for enp0s8 ")
 		with open ("/etc/network/interfaces", "w") as f:
 			f.write(f'''auto enp0s3
 iface enp0s3 inet static
@@ -34,7 +34,7 @@ iface enp0s3 inet static
 	netmask {netmask1}
 auto enp0s8
 iface enp0s8 inet static
-        address {addr2}
-        netmask {netmask2}''')
+	address {addr2}
+	netmask {netmask2}''')
 os.system("systemctl restart networking.service")
 print("Network setup is complete ")
